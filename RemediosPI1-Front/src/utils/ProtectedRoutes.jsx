2d601bbced73638346988dcf3549cf47589d829e
Bernardo Lima <bernardo.lima.93@hotmail.com>
2024-10-08 21:28:07 -0300

    :bug: fix: Correcoes do ProtectedRoutes

--- RemediosPI1-Front/src/utils/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom"
-
-const ProtectedRoutes = () => {
-  const user = true   //To do: Context API, user verification, geting from API / Cookie
-
-  return user ? <Outlet/> : <Navigate to="/login"/>
-}
-
-export default ProtectedRoutes
