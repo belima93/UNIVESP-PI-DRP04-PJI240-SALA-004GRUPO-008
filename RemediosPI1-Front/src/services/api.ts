b981a0664cdb8ec28df27e9f638fcd522d165c98
Bernardo Lima <bernardo.lima.93@hotmail.com>
2024-11-13 21:48:32 -0300

    :bug: fix: Correcoes das api para cloud

--- RemediosPI1-Front/src/services/api.ts
+++ RemediosPI1-Front/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const api = axios.create({
   //baseURL: "http://localhost:8080",
-  baseURL: "http://98.83.145.222:8080"
+  baseURL: "http://50.17.120.87:8080"
 })
