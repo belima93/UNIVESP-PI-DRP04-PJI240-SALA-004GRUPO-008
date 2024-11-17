d04c3bc92d3be9db2f47afe43c1f23b5f6f4ef1d
Bernardo Lima <bernardo.lima.93@hotmail.com>
2024-11-17 17:43:17 -0300

    :bug: fix: Correcoes de IP

--- RemediosPI1-Front/src/services/api.ts
+++ RemediosPI1-Front/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const api = axios.create({
   //baseURL: "http://localhost:8080",
-  baseURL: "http://107.21.180.165:8080"
+  baseURL: "http://34.228.15.39:8080"
 })
