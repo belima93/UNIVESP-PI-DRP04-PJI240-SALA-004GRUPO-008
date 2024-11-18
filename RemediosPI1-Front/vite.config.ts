932cbb5aa41fe46ec50d8690521ddad0f31e7171
Bernardo Lima <bernardo.lima.93@hotmail.com>
2024-11-17 21:15:14 -0300

    :bug: fix: Correcoes de IP

--- RemediosPI1-Front/vite.config.ts
+++ RemediosPI1-Front/vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Permite conexões externas
+    port: 5173,       // Especifica a porta
+    strictPort: true, // Garante que use especificamente esta porta
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  }
 })
